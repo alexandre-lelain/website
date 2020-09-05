3a5a1e259793d175f3367b3c9c2d337964e7630b
Alexandre Le Lain <lelain.alexandre@gmail.com>
2020-09-05 16:24:15 +0200

    feat(layout): set default background color for layout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,11 @@ export const onRenderBody = ({ setBodyAttributes, setHtmlAttributes }) => {
     style: { margin: "0" },
   })
   setHtmlAttributes({
-    style: { margin: "0", scrollBehavior: "smooth", scrollPaddingTop: "64px" },
+    style: {
+      margin: "0",
+      scrollBehavior: "smooth",
+      scrollPaddingTop: "64px",
+      backgroundColor: "#e6e6e6",
+    },
   })
 }
